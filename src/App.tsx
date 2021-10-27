58a9b3deadcc195930614b31ab9de03dccc1c2bd
Mohamed Abuthalha <mohdabueee@gmail.com>
2021-10-27 17:03:40 +0530

    New Commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css';
+import NewComponent from './components/NewComponent/NewComponent';
+
+function App() {
+  return (
+    <div className="App">
+     <NewComponent/>
+    </div>
+  );
+}
+
+export default App;
